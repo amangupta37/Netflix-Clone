6cd86a080dfc3458e0345353913ae79560a8d926
amangupta37 <amangupta6121998@gmail.com>
2021-06-09 13:37:47 +0530

    fix : added new firebase dependencie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
-
 const App = () => {
   return (
     <Router>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
