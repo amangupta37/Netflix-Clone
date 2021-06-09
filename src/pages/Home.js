2b59f5bc24ad456a079a3c86ea74c30f11abd2dd
amangupta37 <amangupta6121998@gmail.com>
2021-06-09 08:25:42 +0530

    fix: imported header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
 import Footer from "../components/Footer/Footer";
 import Faqs from "../components/Faqs/Faqs";
+import Header from "../components/Header/Header";
 const Home = () => {
   return (
     <div className="home">
+      <Header />
       <Jumbotron />
       <Faqs />
       <Footer />
