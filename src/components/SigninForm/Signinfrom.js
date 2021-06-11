910e233d43e950db6298d3db4526606d9b75d468
amangupta37 <amangupta6121998@gmail.com>
2021-06-11 07:59:49 +0530

    imporved : Style of sigin form

---
+++ src/components/SigninForm/Signinfrom.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, FormContainer  } from "./Styles/Signup-Style";
+import Theme from "../Theme/Theme";
+
+const Signupform = () => {
+  return (
+    <>
+      <Theme />
+      <Container>
+        <span>Sign In</span>
+        <FormContainer>
+          <form>
+            <input
+              id="username"
+              type="text"
+              placeholder="Email or phone number"
+              autoComplete="off"
+              required
+            />
+            <input id="Password" type="password" placeholder="Password" required/>
+
+            <button type="submit">Sign In</button>
+
+            <p>New to Netflix ? <span>Sign up now</span></p>
+          </form>
+        </FormContainer>
+       
+      </Container>
+    </>
+  );
+};
+
+export default Signupform;
