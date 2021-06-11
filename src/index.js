0f29de6ea0df8c61c0359dbaa0c903d910bab6e9
amangupta37 <amangupta6121998@gmail.com>
2021-06-11 11:27:00 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css";
 import { GlobalStyles } from "./global-styles";
-import { firebase } from "./lib/firebase.prod";
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
