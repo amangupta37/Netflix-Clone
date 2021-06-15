3f08ad9c3a55380d38ddd77d6013ae19f0c0716e
amangupta37 <amangupta6121998@gmail.com>
2021-06-15 17:04:59 +0530

    added : Banner component

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
-
+import Banner from "../components/Banner/Banner";
 const Browse = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 };
