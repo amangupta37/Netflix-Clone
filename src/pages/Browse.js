10c6b64e33294c23966b6ce72ecaf808965573ed
amangupta37 <amangupta6121998@gmail.com>
2021-06-17 21:01:31 +0530

    added :footer components

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import Shows from "../components/Shows/Shows";
+import Footer from "../components/Footer/Footer";
 const Browse = () => {
   return (
     <div>
       <Navbar />
       <Banner />
       <Shows />
+      <Footer />
     </div>
   );
 };
