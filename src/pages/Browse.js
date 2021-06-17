539651722731b159baa8c6796ee3e3b5299bf14c
amangupta37 <amangupta6121998@gmail.com>
2021-06-17 19:40:36 +0530

    added : Shows Components

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
+import Shows from "../components/Shows/Shows";
 const Browse = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Shows />
     </div>
   );
 };
