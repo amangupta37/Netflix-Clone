7158895b1ae4c7869fc593c2a1d41c564523f0c6
amangupta37 <amangupta6121998@gmail.com>
2021-06-17 20:53:39 +0530

    added : styles for different components

--- src/components/Shows/Styles/Shows-Styles.js
+++ src/components/Shows/Styles/Shows-Styles.js
@@ -5,4 +5,42 @@ export const Container = styled.div`
 `;
 export const ShowsContainer = styled.div`
   width: 100%;
+  background: red;
+  display: grid;
+  grid-template-rows: 280px repeat(6, 200px);
+`;
+
+export const NetflixOriginalContainer = styled.div`
+  width: 100%;
+  background: yellow;
+`;
+
+export const TrendingContainer = styled.div`
+  width: 100%;
+  background: green;
+`;
+
+export const TopRatedContainer = styled.div`
+  width: 100%;
+  background: pink;
+`;
+
+export const ActionContainer = styled.div`
+  width: 100%;
+  background: lime;
+`;
+
+export const ComedyContainer = styled.div`
+  width: 100%;
+  background: cyan;
+`;
+
+export const HorrorContainer = styled.div`
+  width: 100%;
+  background: purple;
+`;
+
+export const RomanceContainer = styled.div`
+  width: 100%;
+  background: white;
 `;
