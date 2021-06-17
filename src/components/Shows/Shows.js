966c37dc41edbea4f41d7387ced191be7bddc271
amangupta37 <amangupta6121998@gmail.com>
2021-06-17 20:52:59 +0530

    added : components from ShowsSection

--- src/components/Shows/Shows.js
+++ src/components/Shows/Shows.js
@@ -1,9 +1,50 @@
 import React from "react";
-import { Container, ShowsContainer } from "./Styles/Shows-Styles";
+import {
+  Container,
+  ShowsContainer,
+  NetflixOriginalContainer,
+  TrendingContainer,
+  TopRatedContainer,
+  ActionContainer,
+  ComedyContainer,
+  HorrorContainer,
+  RomanceContainer,
+} from "./Styles/Shows-Styles";
+
+import Netflix from "./ShowsSection/NetflixOriginal/Netflix";
+import Trending from "./ShowsSection/Trending/Trending";
+import Action from "./ShowsSection/Action/Action";
+import Toprated from "./ShowsSection/TopRated/Toprated";
+import Comedy from "./ShowsSection/Comedy/Comedy";
+import Horror from "./ShowsSection/Horror/Horror";
+import Romance from "./ShowsSection/Romance/Romance";
+
 const Shows = () => {
   return (
     <Container>
-      <ShowsContainer></ShowsContainer>
+      <ShowsContainer>
+        <NetflixOriginalContainer>
+          <Netflix />
+        </NetflixOriginalContainer>
+        <TrendingContainer>
+          <Trending />
+        </TrendingContainer>
+        <TopRatedContainer>
+          <Toprated />
+        </TopRatedContainer>
+        <ActionContainer>
+          <Action />
+        </ActionContainer>
+        <ComedyContainer>
+          <Comedy />
+        </ComedyContainer>
+        <HorrorContainer>
+          <Horror />
+        </HorrorContainer>
+        <RomanceContainer>
+          <Romance />
+        </RomanceContainer>
+      </ShowsContainer>
     </Container>
   );
 };
