50a60c6e5f1890a178c21babdc231e167f5ce7da
amangupta37 <amangupta6121998@gmail.com>
2021-06-17 19:41:51 +0530

    created : Shows components

---
+++ src/components/Shows/Shows.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, ShowsContainer } from "./Styles/Shows-Styles";
+const Shows = () => {
+  return (
+    <Container>
+      <ShowsContainer></ShowsContainer>
+    </Container>
+  );
+};
+
+export default Shows;
