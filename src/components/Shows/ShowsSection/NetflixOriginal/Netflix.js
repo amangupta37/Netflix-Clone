74eb80c1e0da2f5d3877d44b9cb9d9b0e2ba5bc8
amangupta37 <amangupta6121998@gmail.com>
2021-06-18 05:47:48 +0530

    implemented : API call for Netflix component

--- src/components/Shows/ShowsSection/NetflixOriginal/Netflix.js
+++ src/components/Shows/ShowsSection/NetflixOriginal/Netflix.js
@@ -1,7 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  TitleContainer,
+  ImageContainer,
+  NetflixOriginalContainer,
+} from "./Styles/Netflix-Styles";
+import axios from "../../../../API/axios";
+import requests from "../../../../API/Requests";
 const Netflix = () => {
-  return <div>1</div>;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const RequestImage = async () => {
+      const request = await axios.get(requests.fetchTrending);
+
+      console.log(request.data.results);
+
+      setImages(request.data.results);
+    };
+    RequestImage();
+  }, []);
+  console.log(
+    images.map((val) => {
+      return val.id;
+    })
+  );
+
+  return (
+    <Container>
+      <TitleContainer>
+        <h2>NETFLIX ORIGINALS</h2>
+      </TitleContainer>
+      <ImageContainer>
+        {images
+          .slice(0)
+          .reverse()
+          .map((val) => {
+            return (
+              <NetflixOriginalContainer key={val.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${val.poster_path}`}
+                  alt="nn"
+                />
+              </NetflixOriginalContainer>
+            );
+          })}
+      </ImageContainer>
+    </Container>
+  );
 };
 
 export default Netflix;
