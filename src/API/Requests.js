5b30da220a1ae6b864ffec23ea3459d448b92fe7
amangupta37 <amangupta6121998@gmail.com>
2021-06-19 19:58:30 +0530

    updated : new api path

--- src/API/Requests.js
+++ src/API/Requests.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.REACT_APP_TMDB_API;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/upcoming?api_key=${API_KEY}&language=en-IN`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
