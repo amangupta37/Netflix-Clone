80f27af11c496e91bab3069dc42e07f922d2bb08
amangupta37 <amangupta6121998@gmail.com>
2021-07-12 18:45:08 +0530

    improved : FAQ section responsiveness for desktop

--- src/components/Faqs/styles/Faqs-Style.js
+++ src/components/Faqs/styles/Faqs-Style.js
@@ -2,16 +2,22 @@ import styled from "styled-components";
 
 export const Faqcontainer = styled.div`
   width: 100%;
-  //   background: red;
 
   border-top: 0.3rem solid #333333;
+
+  @media only screen and (min-width: 1200px) and (max-width: 1920px) {
+    display: grid;
+    grid-row-gap: 2.5rem;
+  }
 `;
 
 export const Title = styled.div`
   width: 100%;
   margin-top: 2rem;
-  //   background: red;
   text-align: center;
   color: rgb(255, 255, 255);
   font-size: 0.8rem;
+  @media only screen and (min-width: 1200px) and (max-width: 1920px) {
+    font-size: 1.5rem;
+  }
 `;
